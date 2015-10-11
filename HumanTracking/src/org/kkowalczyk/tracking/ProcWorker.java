8cd4f7c847397d6b187617304e12d4bb264833a0
Krzysztof Kowalczyk <krzysiek264@gmail.com>
2015-10-12 00:02:52 +0200

    Wprowadzenie algorytmu ORB, pierwsza iteracja

--- HumanTracking/src/org/kkowalczyk/tracking/ProcWorker.java
+++ HumanTracking/src/org/kkowalczyk/tracking/ProcWorker.java
@@ -1,16 +1,23 @@
 package org.kkowalczyk.tracking;
 
+import org.opencv.core.KeyPoint;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfKeyPoint;
+import org.opencv.core.Scalar;
+import org.opencv.features2d.FeatureDetector;
+import org.opencv.imgproc.Imgproc;
 
 public class ProcWorker implements Runnable{
 	private ThreadManager manager;
 	private Mat target, source;
 	private boolean cloned = true;
+	private FeatureDetector detector;
 	
 	public ProcWorker(ThreadManager mManager, Mat mSource, Mat mTarget){
 		manager=mManager;
 		source=mSource;
 		target=mTarget;
+		detector = FeatureDetector.create(FeatureDetector.ORB);
 	}
 	
 	public boolean isCloning(){
@@ -27,6 +34,11 @@ public class ProcWorker implements Runnable{
 		cloned=true;
 		synchronized(manager.camera_lock){
 			manager.camera_lock.notify();
+		}
+		MatOfKeyPoint points = new MatOfKeyPoint();
+		detector.detect(target, points);
+		for(KeyPoint p : points.toList()){
+			Imgproc.circle(target, p.pt, 5, new Scalar(0,255,0), 3);
 		}
 		manager.drawPost();
 	}
